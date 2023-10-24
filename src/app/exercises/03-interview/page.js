a516c431f6282a5c3eb3c251bf8fe84ba3363007
Tobias Basteiro <tobiasbasteiro@Tobiass-MBP.attlocal.net>
2023-10-24 12:25:24 -0400

    nextjs: exercise 3 (interview)

--- src/app/exercises/03-interview/page.js
+++ src/app/exercises/03-interview/page.js
@@ -1,35 +1,32 @@
-'use client';
-import React from 'react';
-import { useMediaQuery } from 'react-responsive';
+"use client";
+import React from "react";
+import { useMediaQuery } from "react-responsive";
 
-import Interview from './Interview';
-import './styles.css';
+import Interview from "./Interview";
+import "./styles.css";
 
 function InterviewExercise() {
+  const [sidebarClass, setSidebarClass] = React.useState("");
   const isDesktop = useMediaQuery({
-    query: '(min-width: 500px)',
+    query: "(min-width: 900px)",
   });
 
+  React.useEffect(() => {
+    setSidebarClass(isDesktop ? "aside-desktop" : "aside-mobile");
+  });
   return (
     <main>
       <Interview />
-      {isDesktop && (
-        <aside>
-          <img
-            src="/gwen-artist.png"
-            alt="Portrait of the artist"
-          />
-          <h2>About the Artist</h2>
-          <p>
-            Gwen Altaria is a contemporary artist known for
-            her unique blend of traditional oil painting
-            techniques and pop culture references. Born and
-            raised in London, England, Vivi had an early
-            love for both the fine arts and the fantastical
-            worlds of video games.
-          </p>
-        </aside>
-      )}
+      <aside className={sidebarClass}>
+        <img src="/gwen-artist.png" alt="Portrait of the artist" />
+        <h2>About the Artist</h2>
+        <p>
+          Gwen Altaria is a contemporary artist known for her unique blend of
+          traditional oil painting techniques and pop culture references. Born
+          and raised in London, England, Vivi had an early love for both the
+          fine arts and the fantastical worlds of video games.
+        </p>
+      </aside>
     </main>
   );
 }
