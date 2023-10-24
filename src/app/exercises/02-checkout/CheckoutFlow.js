0b3e43ffd2448a62487d483d06052a6013b44887
Tobias Basteiro <tobiasbasteiro@Tobiass-MBP.attlocal.net>
2023-10-24 11:56:01 -0400

    nextjs: exercise 2 (checkout)

--- src/app/exercises/02-checkout/CheckoutFlow.js
+++ src/app/exercises/02-checkout/CheckoutFlow.js
@@ -1,13 +1,18 @@
-'use client';
-import React from 'react';
+"use client";
+import React from "react";
 
-import CartTable from './CartTable';
+import CartTable from "./CartTable";
+import Spinner from "../../../components/Spinner";
 
-function CheckoutFlow({
-  items,
-  taxRate,
-  handleDeleteItem,
-}) {
+function CheckoutFlow({ items, taxRate, handleDeleteItem }) {
+  if (items === null) {
+    return (
+      <div className="checkout-flow empty">
+        <Spinner />{" "}
+      </div>
+    );
+  }
+
   if (items.length === 0) {
     return (
       <div className="checkout-flow empty">
@@ -15,10 +20,9 @@ function CheckoutFlow({
       </div>
     );
   }
-
-  const priceFormatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
+  const priceFormatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
   });
 
   const subtotal = calculateSubtotal(items);
@@ -27,10 +31,7 @@ function CheckoutFlow({
 
   return (
     <div className="checkout-flow">
-      <CartTable
-        items={items}
-        handleDeleteItem={handleDeleteItem}
-      />
+      <CartTable items={items} handleDeleteItem={handleDeleteItem} />
 
       <table className="checkout-totals">
         <tbody>
--- src/app/exercises/02-checkout/page.js
+++ src/app/exercises/02-checkout/page.js
@@ -1,17 +1,28 @@
-'use client';
-import React from 'react';
+"use client";
+import React from "react";
 
-import DATA from './data';
-import reducer from './reducer';
-import StoreItem from './StoreItem';
-import CheckoutFlow from './CheckoutFlow';
-import './styles.css';
+import DATA from "./data";
+import reducer from "./reducer";
+import StoreItem from "./StoreItem";
+import CheckoutFlow from "./CheckoutFlow";
+import "./styles.css";
 
 function CheckoutExercise() {
-  const [items, dispatch] = React.useReducer(
-    reducer,
-    []
-  );
+  const [items, dispatch] = React.useReducer(reducer, null);
+
+  React.useEffect(() => {
+    const savedCart = window.localStorage.getItem("saved-cart");
+    dispatch({
+      type: "initialize-cart",
+      initialCart: savedCart === null ? [] : JSON.parse(savedCart),
+    });
+  }, []);
+
+  React.useEffect(() => {
+    if (items !== null) {
+      window.localStorage.setItem("saved-cart", JSON.stringify(items));
+    }
+  });
 
   return (
     <>
@@ -25,7 +36,7 @@ function CheckoutExercise() {
               item={item}
               handleAddToCart={(item) => {
                 dispatch({
-                  type: 'add-item',
+                  type: "add-item",
                   item,
                 });
               }}
@@ -38,7 +49,7 @@ function CheckoutExercise() {
           taxRate={0.15}
           handleDeleteItem={(item) =>
             dispatch({
-              type: 'delete-item',
+              type: "delete-item",
               item,
             })
           }
